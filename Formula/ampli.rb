34728b3745bf903b5c1e54bbfd8229a3324b372f
Andrey Sokolov <falconandy@yandex.ru>
2021-10-10 10:16:08 +0400

    AMP-42910 Added 'ampli' and 'ampli-dev' formulae

---
+++ Formula/ampli.rb
@@ -0,0 +1,20 @@
+class Ampli < Formula
+  desc "The Ampli CLI"
+  homepage "https://amplitude.com"
+  url ""
+  version ""
+  sha256 ""
+
+  depends_on "node@12"
+
+  def install
+    inreplace "bin/ampli", /^CLIENT_HOME=/, "export AMPLI_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    inreplace "bin/ampli", "\"$DIR/node\"", "#{Formula["node@12"].bin}/node"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/ampli" => "ampli"
+  end
+
+  test do
+    system bin/"ampli", "version"
+  end
+end
