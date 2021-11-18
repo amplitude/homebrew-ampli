74c2205f7510a2524aa9db1824d226019096a42b
justin-fiedler <justin-fiedler@users.noreply.github.com>
2021-11-18 18:50:58 +0000

    v1.0.1-master-20211118184928.0

--- Formula/ampli-dev.rb
+++ Formula/ampli-dev.rb
@@ -1,9 +1,9 @@
 class AmpliDev < Formula
   desc "The Ampli CLI (Dev)"
   homepage "https://amplitude.com"
-  url "https://github.com/amplitude/homebrew-ampli/releases/download/v1.0.1-master-20211115190614.0/ampli-v1.0.1-master-20211115190614.0.tar.gz"
-  version "1.0.1-master-20211115190614.0"
-  sha256 "5d21d1adb9c363678bcebdb48f50b6b14629bc6b43308265ac32302a20b699d3"
+  url "https://github.com/amplitude/homebrew-ampli/releases/download/v1.0.1-master-20211118184928.0/ampli-v1.0.1-master-20211118184928.0.tar.gz"
+  version "1.0.1-master-20211118184928.0"
+  sha256 "a7fdd622612e55ec6d46298cb3c912249971a97d513ea56fd547b190529eb37f"
 
   depends_on "node@12"
 
