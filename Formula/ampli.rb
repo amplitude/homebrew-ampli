f9254f5e098f4e52105d98ac98305ef3eff8646c
falconandy <falconandy@users.noreply.github.com>
2022-04-01 01:47:48 +0000

    v1.5.0

--- Formula/ampli.rb
+++ Formula/ampli.rb
@@ -1,9 +1,9 @@
 class Ampli < Formula
   desc "The Ampli CLI"
   homepage "https://amplitude.com"
-  url "https://github.com/amplitude/homebrew-ampli/releases/download/v1.4.4/ampli-v1.4.4.tar.gz"
-  version "1.4.4"
-  sha256 "75b69e1cf62708ff9eef01fe2b2ae2534704fdbcdc23077890c01995417e0b5b"
+  url "https://github.com/amplitude/homebrew-ampli/releases/download/v1.5.0/ampli-v1.5.0.tar.gz"
+  version "1.5.0"
+  sha256 "e0e74d6f13895074b86c3ef636f6629bd312d4a8e0b7169767f39218345a2846"
 
   depends_on "node@12"
 
