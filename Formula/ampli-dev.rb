30e60d5ce97c661c08ab9af002d7c9dcd9e2f4ab
shibrady <shibrady@users.noreply.github.com>
2025-02-20 05:41:18 +0000

    v1.36.1-master-20250220054030.0

--- Formula/ampli-dev.rb
+++ Formula/ampli-dev.rb
@@ -1,9 +1,9 @@
 class AmpliDev < Formula
   desc "The Ampli CLI (Dev)"
   homepage "https://amplitude.com"
-  url "https://github.com/amplitude/homebrew-ampli/releases/download/v1.36.1-master-20250220051909.0/ampli-v1.36.1-master-20250220051909.0.tar.gz"
-  version "1.36.1-master-20250220051909.0"
-  sha256 "200d37075a86f10d9fccf47e0250253dd5984da46eb93c67fc537c4a6bf25c1e"
+  url "https://github.com/amplitude/homebrew-ampli/releases/download/v1.36.1-master-20250220054030.0/ampli-v1.36.1-master-20250220054030.0.tar.gz"
+  version "1.36.1-master-20250220054030.0"
+  sha256 "e547647afe6130fb42de4e6702cf486c4ff70900cb1ba07bc81e9a4a6b5548c4"
 
   depends_on "node@18"
 
